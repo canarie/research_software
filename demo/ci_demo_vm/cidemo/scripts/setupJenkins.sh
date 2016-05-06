5517227e05d68ab575a1194ccf1abf5b0aa1dcdf
Rob Young <rob.young@canarie.ca>
2016-05-06 17:19:56 -0400

    RSR-1442 doc update.

--- demo/ci_demo_vm/cidemo/scripts/setupJenkins.sh
+++ demo/ci_demo_vm/cidemo/scripts/setupJenkins.sh
@@ -43,6 +43,7 @@ docker run -d -p 8080:8080 -p 50000:50000 --name jenkins --restart always -v /ho
 sudo yum -y install java-1.8.0-openjdk wget
 
 # TODO install java jar correctly
+# could use https://github.com/jwilder/dockerize here
 sleep 10
 wget -O /home/vagrant/slave.jar http://localhost:8080/jnlpJars/slave.jar
 java -jar /home/vagrant/slave.jar -jnlpUrl http://localhost:8080/computer/Host_Node/slave-agent.jnlp > /dev/null &
